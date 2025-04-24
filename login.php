8716f12937717163e6333944c0175305294b0dcd
dalvero <dalvero589@gmail.com>
2025-04-24 11:14:56 +0700

    Tambahkan H1 dan ganti nama file

--- login.php
+++ login.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dad Project</title>
 </head>
+
 <body>
     <!-- Haiii sobat -->
+    <h1>Hello Dad Project</h1>
 </body>
+
 </html>
