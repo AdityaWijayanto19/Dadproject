b8c58dcd828c1209efb7c9cb459866617d0f6629
me-dav <000david12345go@gmail.com>
2025-04-28 15:21:45 +0700

    bikin crud manage kelas

--- admin/manageClasses.php
+++ admin/manageClasses.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require '../koneksi/koneksi.php';
 
 $data = query("SELECT * FROM `kelas`");
@@ -6,6 +6,7 @@ $data = query("SELECT * FROM `kelas`");
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,9 +14,11 @@ $data = query("SELECT * FROM `kelas`");
     <link rel="shortcut icon" href="picture/logo (1).png" type="image/x-icon">
     <title>Kelola Kelas-DadProject</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="adminDashboard.css">
     <link rel="stylesheet" href="../css/kelas.css">
 </head>
+
 <body>
     <div class="wrapper">
         <!-- Sidebar -->
@@ -44,9 +47,9 @@ $data = query("SELECT * FROM `kelas`");
             </header>
 
             <div class="boxbtn">
-                <button><a href="#">Tambah kelas</a></button>
+                <button><a href="addClasses.php">Tambah kelas</a></button>
             </div>
-            
+
             <div>
                 <table border="1" cellpadding="10" cellspacing="0">
                     <tr>
@@ -58,24 +61,43 @@ $data = query("SELECT * FROM `kelas`");
                         <th>ID mentor</th>
                         <th>Kategori</th>
                     </tr>
-                    <?php $i=1; ?>
-                    <?php foreach($data as $dt) : ?>
-                    <tr>
-                        <td><?= $i; ?></td>
-                        <td>
-                            <button class="hapus"><a href="#">Hapus</a></button>
-                            <button class="edit"><a href="#">Edit</a></button>
-                        </td>
-                        <td><?= $dt["title_kelas"] ?></td>
-                        <td><?= $dt["foto"] ?></td>
-                        <td><?= $dt["desk_kelas"] ?></td>
-                        <td><?= $dt["mentor_id"] ?></td>
-                        <td><?= $dt["kategori"] ?></td>
-                    </tr>
-                    <?php $i++ ?>
+                    <?php $i = 1; ?>
+                    <?php foreach ($data as $dt): ?>
+                        <tr>
+                            <td><?= $i; ?></td>
+                            <td>
+                                <button class="hapus"><a href="#" onclick="confirm(<?= $dt['kelas_id'] ?>)">Hapus</a></button>
+                                <button class="edit"><a href="editKelas.php?id=<?= $dt['kelas_id']; ?>">Edit</a></button>
+                            </td>
+                            <td><?= $dt["title_kelas"] ?></td>
+                            <td><?= $dt["foto"] ?></td>
+                            <td><?= $dt["desk_kelas"] ?></td>
+                            <td><?= $dt["mentor_id"] ?></td>
+                            <td><?= $dt["kategori"] ?></td>
+                        </tr>
+                        <?php $i++ ?>
                     <?php endforeach; ?>
                 </table>
             </div>
         </div>
+
+        <script>
+            function confirm(id) {
+                Swal.fire({
+                    title: "yakin ?",
+                    text: "data akan dihapus permanent!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "ya, hapus!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = 'hapusKelas.php?id=' + id;
+                    }
+                });
+            }
+        </script>
 </body>
+
 </html>
